ae5429951442bd1c91ef5b1c3190e66f2ae82acc
JoshLaunchcode <holo.sf0@gmail.com>
2021-08-01 22:19:39 -0500

    working on Angular Exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Cool Pictures';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://newevolutiondesigns.com/images/freebies/cool-wallpaper-1.jpg';
+  image3 = 'https://wallpapercave.com/wp/wp2551790.jpg';
 
   constructor() { }
 
